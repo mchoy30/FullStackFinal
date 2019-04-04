ffe60352d925f8f7ed6b2aed4d790a14722b306a
Mackenzie <mchoy30@academic.rrc.ca>
2019-04-04 14:59:04 -0500

    Fixed model for active admin

--- finalProject/db/migrate/20190404194252_create_products.rb
+++ finalProject/db/migrate/20190404194252_create_products.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :productID
       t.string :name
       t.string :sescription
-      t.String :price
+      t.string :price
       t.string :category
 
       t.timestamps
